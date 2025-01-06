d84f064caa386a1876a496ddd1eb536fce2f85ca
melv121 <melvinpradier@gmail.com>
2025-01-06 15:43:55 +0100

    ajout carte

---
+++ mvc/View/recette_card.php
@@ -0,0 +1,10 @@
+require ("includes/database.php");
+$pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">recette</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
